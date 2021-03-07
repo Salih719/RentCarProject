cc4692a48bd5f6b41657830332ee571478853840
Salih Kurt <matrixmehmett@hotmail.com>
2021-03-07 05:30:22 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,49 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get All
+            CarManager carManager = new CarManager(new EfCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+
+
+            //GetCarsByBrandId
+            Console.WriteLine("----------");
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.CarName + " : " + car.Description);
+            }
+
+
+            Console.WriteLine("----------Bütün Renkleri sırala----------");
+
+
+            ColorManager colorManager = new ColorManager(new EfColorDal());
+            foreach (var color in colorManager.GetAll())
+            {
+                Console.WriteLine(color.ColorId + " : " + color.ColorName);
+            }
+
+            Console.WriteLine("----------Markaların id leri----------");
+
+
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+            foreach (var brand in brandManager.GetAll())
+            {
+                Console.WriteLine(brand.BrandId + " : " + brand.BrandName);
+            }
+
+
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,36 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Linq;
+
+namespace DataAccess.Concrete
+{
+    public class EfCarDal : EfEntityRepositoryBase<Car, NorthwindContext>, ICarDal
+    {
+        public List<CarDetailDto> GetCarDetails()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var result = from car in context.Cars
+                             join color in context.Colors
+                             on car.ColorId equals color.ColorId
+                             join brand in context.Brands
+                             on car.BrandId equals brand.BrandId
+                             select new CarDetailDto
+                             {
+                                 CarName = car.CarName,
+                                 BrandName = brand.BrandName,
+                                 ColorName = color.ColorName,
+                                 DailyPrice = car.DailyPrice
+                             };
+                return result.ToList();
+            }
+        }
+    }
+}
